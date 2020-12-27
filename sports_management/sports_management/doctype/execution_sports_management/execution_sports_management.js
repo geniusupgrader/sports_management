75517389f1b52a00d57f9147d8910f4212dec410
Robin Rosenstock <robin.rosenstock@t-online.de>
2020-12-27 14:35:49 +0100

    Custom Script to code

--- sports_management/sports_management/doctype/execution_sports_management/execution_sports_management.js
+++ sports_management/sports_management/doctype/execution_sports_management/execution_sports_management.js
@@ -2,7 +2,106 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Execution_sports_management', {
-	// refresh: function(frm) {
-
-	// }
+	refresh(frm) {
+		// your code here
+	}
 });
+
+	frappe.ui.form.on("Execution_sports_management", {
+	"exercise": function(frm) {
+	
+		if (frm.doc.exercise_type !== "" && frm.doc.exercise_group !== ""){
+		frm.fields_dict['exercise'].get_query = function() {
+			return {
+				filters: {
+					'exercise_type': frm.doc.exercise_type,
+					'exercise_series': frm.doc.exercise_series
+				}
+			};
+		};  
+		}else{
+		
+		if (frm.doc.exercise_type === "" && frm.doc.exercise_group !== ""){
+		frm.fields_dict['exercise'].get_query = function() {
+			return {
+				filters: {
+					'exercise_series': frm.doc.exercise_series
+				}
+			};
+		};  
+		}
+		
+		if (frm.doc.exercise_type !== "" && frm.doc.exercise_group === ""){
+		frm.fields_dict['exercise'].get_query = function() {
+			return {
+				filters: {
+					'exercise_type': frm.doc.exercise_type
+				}
+			};
+		};  
+		}
+		}
+	
+	}
+	});
+	
+	
+	// for updating the image, when exercise is selected
+	frappe.ui.form.on("Execution_sports_management", {
+	'exercise': function(frm) {
+		frappe.db.get_value('Exercise_sports_management', frm.doc.exercise, 'attach_the_image')
+		.then(r => {
+			cur_frm.set_value('attach_image', r.message.attach_the_image)
+		})
+	frm.refresh()
+	}
+	});
+	
+	
+
+	frappe.ui.form.on("Execution_sports_management", {
+	refresh: function(frm) {
+		frm.add_custom_button(__("Start"), function() {
+			frm.set_value('time', frappe.datetime.now_time());
+		}); 
+	}
+	});
+
+	
+
+	frappe.ui.form.on("Execution_sports_management", {
+	refresh: function(frm) {
+		frm.add_custom_button(__("Stop"), function() {
+			frm.set_value('stop_time', frappe.datetime.now_time());
+		}); 
+	}
+	});
+
+	
+	
+	// Achtung, komischerweise, lässt sich dann nicht mehr in der App abspeichern?
+	// Überprüfung beim Speichern, ob Stop Zeit später ist als Start Zeit
+	frappe.ui.form.on("Execution_sports_management", {
+	validate: function(frm) {
+		if(frm.doc.stop_time < frm.doc.time) {
+			msgprint('Stop Time must be later');
+			validated = false;
+		}
+	}
+	});
+	
+	
+	
+	/* Versuch um vom Table Multiselect zu filtern (muscles)
+	frappe.ui.form.on("Execution_sports_management", {
+	onload: function(frm) {
+	cur_frm.fields_dict.exercise.get_query = function(doc,cdt,cdn) {
+			return {
+				filters:[
+					['muscles', '=', "Rücken"],
+					//['exercise_type', '=', "Krafttraining"]
+				]
+			}
+		}}});
+		
+	*/
--- sports_management/sports_management/doctype/exercise_sports_management/exercise_sports_management.js
+++ sports_management/sports_management/doctype/exercise_sports_management/exercise_sports_management.js
@@ -2,7 +2,23 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Exercise_sports_management', {
-	// refresh: function(frm) {
-
-	// }
+	refresh(frm) {
+		// your code here
+	}
 });
+
+
+
+frappe.ui.form.on("Exercise_sports_management", {
+refresh: function(frm) {
+    frm.add_custom_button(__("Create"), function() {
+        var new_exercise_doc = frappe.model.get_new_doc('Execution_sports_management');
+		new_exercise_doc.exercise = frm.doc.name;
+		frappe.set_route('Form', 'Execution_sports_management', new_exercise_doc.name);
+        
+        
+        
+    }); 
+}
+});
+
