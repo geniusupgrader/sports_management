b80ebb403f7df3f2dbf8eedaf007e79c686e5b9a
Robin Rosenstock <robin.rosenstock@t-online.de>
2021-01-03 19:51:56 +0100

    removed some whitespace

--- sports_management/sports_management/doctype/exercise_sports_management/exercise_sports_management.js
+++ sports_management/sports_management/doctype/exercise_sports_management/exercise_sports_management.js
@@ -7,18 +7,12 @@ frappe.ui.form.on('Exercise_sports_management', {
 	}
 });
 
-
-
 frappe.ui.form.on("Exercise_sports_management", {
 refresh: function(frm) {
     frm.add_custom_button(__("Create"), function() {
         var new_exercise_doc = frappe.model.get_new_doc('Execution_sports_management');
 		new_exercise_doc.exercise = frm.doc.name;
 		frappe.set_route('Form', 'Execution_sports_management', new_exercise_doc.name);
-        
-        
-        
     }); 
 }
 });
-
