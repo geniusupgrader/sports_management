2d461512ad6c3b1dbec0cd1e43b0e02d33347274
Robin Rosenstock <robin.rosenstock@t-online.de>
2021-01-05 20:11:57 +0100

    new feature: added button show executions

--- sports_management/sports_management/doctype/exercise_sports_management/exercise_sports_management.js
+++ sports_management/sports_management/doctype/exercise_sports_management/exercise_sports_management.js
@@ -3,10 +3,13 @@
 
 frappe.ui.form.on("Exercise_sports_management", {
 refresh: function(frm) {
-    frm.add_custom_button(__("Create"), function() {
+    frm.add_custom_button(__("Create Execution"), function() {
         var new_exercise_doc = frappe.model.get_new_doc('Execution_sports_management');
 		new_exercise_doc.exercise = frm.doc.name;
 		frappe.set_route('Form', 'Execution_sports_management', new_exercise_doc.name);
+    }); 
+    frm.add_custom_button(__("Show Executions"), function() {
+        frappe.set_route('List', 'Execution_sports_management', {'exercise': frm.doc.name1});
     }); 
 }
 });
